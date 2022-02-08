e71a14112479731faa53aaa9afee1c0a7d5ead85
rodfranco <rodrigo.rfranco@mercadolivre.com>
2022-02-08 14:08:09 -0300

    REQ 1 REVIEW

---
+++ src/main/java/com/mercadolibre/desafiofinalbootcampgrupo2/exception/RepresentativeInvalidException.java
@@ -0,0 +1,10 @@
+package com.mercadolibre.desafiofinalbootcampgrupo2.exception;
+
+public class RepresentativeInvalidException extends RuntimeException{
+
+    private static final long serialVersionUID = -4993205001650484387L;
+
+    public RepresentativeInvalidException(String message) {
+        super(message);
+    }
+}
